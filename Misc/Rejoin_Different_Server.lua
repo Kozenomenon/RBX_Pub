7c192a1797f60f87dd94207f37ea7b714a06a65a
Koz <64675301+Kozenomenon@users.noreply.github.com>
2021-10-01 16:14:58 -0400

    fixes for rejoin script. hardened against failure.

--- Misc/Rejoin_Different_Server.lua
+++ Misc/Rejoin_Different_Server.lua
@@ -1,30 +1,40 @@
 local DelaySeconds = 1
 local ServerListLimit = 100
 
-function RejoinNewServer(teleportDelay: number?,serverLimit: number?)
+function RejoinNewServer(teleportDelay: number,serverLimit: number)
 	teleportDelay = teleportDelay or 1
 	serverLimit = serverLimit or 100
+	serverLimit = math.clamp(serverLimit,1,100) -- rbx api won't allow more than 100
 	local url = ("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Asc&limit=%s"):format(game.PlaceId,tostring(serverLimit))
 
-	-- afaik this will only work with: synapse, script-ware, krnl, and protosmasher
-	local req = (syn and syn.request) or (http and http.request) or http_request or request
-	local Response = req({
-		Url = url,
-		Method = "GET"
-	})
+	-- afaik this will only work with: synapse, script-ware, krnl, and protosmasher. or it will default to the rbx http get. 
+	local req = (syn and syn.request) or (http and http.request) or http_request or request or function() local resp = {}; resp.Body = game:HttpGetAsync(url); return resp.Body end
+	local Response = nil
+	pcall(function() 
+		Response = req({
+			Url = url,
+			Method = "GET"
+		})
+	end)
+	if not Response then print("HTTP Request Call Failed"); return false end
 
 	local currJobId = game.JobId
 	local newJobId = nil
 	local justInCase = 0
-	local respJson = game:GetService("HttpService"):JSONDecode(Response.Body)
-	for i,v in pairs(respJson) do
-		if (i == "data" and v and #v > 0) then
-			while (justInCase < serverLimit and (newJobId == nil or newJobId == currJobId)) do
-				justInCase = justInCase + 1
-				local sel = math.random(1,#v)
-				newJobId = v[sel].id
+	local respJson = nil
+	pcall(function() respJson = game:GetService("HttpService"):JSONDecode(Response.Body) end)
+	if (respJson) then
+		for i,v in pairs(respJson) do
+			if (i == "data" and v and #v > 0) then
+				while (justInCase < serverLimit and (newJobId == nil or newJobId == currJobId)) do
+					justInCase = justInCase + 1
+					local sel = math.random(1,#v)
+					newJobId = v[sel].id
+				end
 			end
 		end
+	else
+		print("Unexpected Response >>>",Response.StatusCode,Response.StatusMessage,Response.Body)
 	end
 
 	if (newJobId ~= nil) then
@@ -38,10 +48,12 @@ function RejoinNewServer(teleportDelay: number?,serverLimit: number?)
 			wait(teleportDelay)
 		end
 		ts:TeleportToPlaceInstance(game.PlaceId,newJobId,p)
-		print("teleported")
+		print("Teleported!")
+		return true
 	else
-		print("failed to get new jobid")
+		print("Failed to get new jobid. Try bigger limit?")
+		return false
 	end
 end
-
-RejoinNewServer(DelaySeconds,ServerListLimit)
+-- will keep trying every 2s until it works. this is meant for example.. do your own thing ofc :P 
+while not RejoinNewServer(DelaySeconds,ServerListLimit) do wait(2) end
