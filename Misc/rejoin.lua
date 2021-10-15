b8ba6a2870bd2f9ff9fa64a67627660b1656787b
Koz <64675301+Kozenomenon@users.noreply.github.com>
2021-10-15 05:58:35 -0400

    rejoin

--- Misc/rejoin.lua
+++ Misc/rejoin.lua
@@ -1,10 +1,20 @@
-local ts = game:GetService('TeleportService')
---ts:Teleport(game.PlaceId)
-
-local origPID = game.PlaceId
-
-local p = game:GetService("Players").LocalPlayer
-
-p:Kick('boo hoo')
-wait(8)
-ts:Teleport(origPID)
+local same = true
+if same then
+    local ts = game:GetService('TeleportService')
+    --ts:Teleport(game.PlaceId)
+    
+    local origPID = game.PlaceId
+    
+    pcall(function()
+        local p = game:GetService("Players").LocalPlayer
+        p:Kick('boo hoo')
+    end)
+    
+    wait(1)
+    ts:Teleport(origPID)
+else
+    -- load it to local var. returns func
+	local rj = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Kozenomenon/RBX_Pub/main/Misc/Rejoin_Different_Server.lua"))()
+	-- call the func w/ defaults
+	rj()    
+end
